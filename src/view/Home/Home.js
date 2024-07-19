4cd1ef4c15f610ccbb98158b08328d7360edde78
rutu1518 <rutujamore1508@gmail.com>
2024-07-19 21:43:36 +0530

    initial commit

---
+++ src/view/Home/Home.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import "./Home.css"
+// import React, { useEffect, useState } from 'react'
+
+import PlantCard from '../../component/PlantCard/PlantCard'
+import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
+
+function Home() {
+
+    const [plants, setPlants] = useState([])
+
+    const loadPlants = async () =>{
+    toast.loading("Loading Plants")
+    const response = await axios.get(`http://localhost:8000/plants`)
+
+    toast.dismiss() 
+
+    toast.success("Plants Loaded Successfullly")
+     setPlants(response.data.data)
+ }
+
+ useEffect(()=>{
+    loadPlants()
+ }, [])
+ 
+
+  return (
+    <div>
+      <h1>Plants</h1>
+
+      {
+        plants.map((plant, i)=>{
+            const {
+                _id, 
+                name, 
+                category, 
+                image, 
+                price, 
+                description
+            } = plant
+
+            return(<PlantCard
+            key={i}
+                 _id={_id} 
+                name={name} 
+                category={category} 
+                image={image} 
+                price={price} 
+                description={description}/>)
+        })
+      }
+    <Toaster/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/view/Plantview/Plantview.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Plantview.css"
+
+function Plantview() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Plantview
